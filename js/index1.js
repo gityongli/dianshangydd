f65c754e65a8282c6e9fe71a80573a0b536b3a3c
gityongli <2508112607@qq.com>
2018-01-09 08:44:13 +0800

    第二次上传

---
+++ js/index1.js
@@ -0,0 +1,73 @@
+/**
+ * Created by lenovo on 18/1/7 007.
+ */
+function li1(){
+    document.getElementById("main1").style.display="block";
+    document.getElementById("main2").style.display="none";
+    document.getElementById("main3").style.display="none";
+    document.getElementById("main4").style.display="none";
+    document.getElementById("main5").style.display="none";
+}
+function li2(){
+    document.getElementById("main2").style.display="block";
+    document.getElementById("main1").style.display="none";
+    document.getElementById("main3").style.display="none";
+    document.getElementById("main4").style.display="none";
+    document.getElementById("main5").style.display="none";
+}
+function li3(){
+    document.getElementById("main3").style.display="block";
+    document.getElementById("main1").style.display="none";
+    document.getElementById("main2").style.display="none";
+    document.getElementById("main4").style.display="none";
+    document.getElementById("main5").style.display="none";
+}
+function li4(){
+    document.getElementById("main4").style.display="block";
+    document.getElementById("main1").style.display="none";
+    document.getElementById("main2").style.display="none";
+    document.getElementById("main3").style.display="none";
+    document.getElementById("main5").style.display="none";
+}
+function li5(){
+    document.getElementById("main5").style.display="block";
+    document.getElementById("main1").style.display="none";
+    document.getElementById("main2").style.display="none";
+    document.getElementById("main3").style.display="none";
+    document.getElementById("main4").style.display="none";
+}
+function li11(){
+    document.getElementById("main11").style.display="block";
+    document.getElementById("main22").style.display="none";
+    document.getElementById("main33").style.display="none";
+    document.getElementById("main44").style.display="none";
+    document.getElementById("main55").style.display="none";
+}
+function li22(){
+    document.getElementById("main22").style.display="block";
+    document.getElementById("main11").style.display="none";
+    document.getElementById("main33").style.display="none";
+    document.getElementById("main44").style.display="none";
+    document.getElementById("main55").style.display="none";
+}
+function li33(){
+    document.getElementById("main33").style.display="block";
+    document.getElementById("main11").style.display="none";
+    document.getElementById("main22").style.display="none";
+    document.getElementById("main44").style.display="none";
+    document.getElementById("main55").style.display="none";
+}
+function li44(){
+    document.getElementById("main44").style.display="block";
+    document.getElementById("main11").style.display="none";
+    document.getElementById("main22").style.display="none";
+    document.getElementById("main33").style.display="none";
+    document.getElementById("main54").style.display="none";
+}
+function li55(){
+    document.getElementById("main55").style.display="block";
+    document.getElementById("main11").style.display="none";
+    document.getElementById("main22").style.display="none";
+    document.getElementById("main33").style.display="none";
+    document.getElementById("main44").style.display="none";
+}
